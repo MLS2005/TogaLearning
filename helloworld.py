b45567ccf11c4427f3c361d5eb116c8ae0a0e99f
Mitchell Schreiber <Mitchell Schreiber>
2025-09-18 16:29:19 -0400

    all functions and classes

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ def build(app):
     box = toga.Box()
 
     button = toga.Button("Hello world", on_press=button_handler)
-    button.style.margin = (20,50,50,50)
+    button.style.margin = (50)
     """
     What is done here is say that the button will have a margin of 50 pixels on all sides. If we wanted to 
     define a margin of 20 pixels on top of the button, we could have defined margin_top = 20, or we could have 
